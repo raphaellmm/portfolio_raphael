6f0458ef1bbad2ac870bc2d79b66ff7eb89defbf
raphaellmm <123992121+raphaellmm@users.noreply.github.com>
2023-02-02 17:59:09 +0100

    changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./about.css";
 import AboutImg from "../../assets/about.jpg";
-import CV from "../../assets/cv_raphael.pdf";
+import CV from "../../assets/cv.pdf";
 import Info from './Info';
 
 const About = () => {
